500fe6bf8324d79554a0a8345d26448e4d9e1542
Alex <sasha.stitkov@rambler.ru>
2021-04-22 12:21:33 +0300

    add user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
 
 module.exports.login = function (req,res) {
     res.status(200).json({
@@ -9,13 +10,28 @@ module.exports.login = function (req,res) {
     })
 }
 
-module.exports.register = function (req,res) {
-    // email, password
-    // валидация аккаунта
-    const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
+module.exports.register = async function (req,res) {
 
-    user.save().then(()=> console.log('User created'))
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        // Пользователь существует отдать ошибку
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой'
+        })
+    } else {
+        // Нужно создать пользователя
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {}
+    }
 }
