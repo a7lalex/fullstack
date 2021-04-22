6b341084921232d0aac3196468f841514ab75a77
Alex <sasha.stitkov@rambler.ru>
2021-04-22 13:30:28 +0300

    add jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,35 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../config/keys')
 
-module.exports.login = function (req,res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function (req,res) {
+    const candidate = await User.findOne({email: req.body.email})
+    if (candidate) {
+        // Провека пользователь существует
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            // Генрация токена
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60 })
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            // Пароли не совпали
+            res.status(401).json({
+                message: 'Пароли не совпадают попробуйте снова'
+            })
         }
-    })
+    } else {
+        // Пользователя нетб ошибка
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден'
+        })
+    }
 }
 
 module.exports.register = async function (req,res) {
