0d7b578135324b07d2ae7c59ca40e90c8bd43225
Alex <sasha.stitkov@rambler.ru>
2021-04-23 09:48:43 +0300

    add models

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,36 @@
-module.exports.getByCategoryId = function (req,res) {
+const Position = require('../models/Position')
+const errorHandler = require('../utils/errorHandler')
 
+module.exports.getByCategoryId = async function (req,res) {
+    try {
+        const position = await Position.find({
+
+        })
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
 module.exports.create = function (req,res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
 module.exports.remove = function (req,res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
 
 module.exports.update = function (req,res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e)
+    }
 }
--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true,
-        unicode: true
+        unique: true
     },
     password: {
         type: String,
