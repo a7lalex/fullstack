772f8b7294952ab8b3e0635920e6c106196d25d4
Alex <sasha.stitkov@rambler.ru>
2021-04-28 10:36:33 +0300

    add form login

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-page',
@@ -7,10 +8,23 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  form!: FormGroup
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+  get f()
+  {
+    return this.form.controls;
   }
 
+  OnSubmit() {
+    console.log('Отправить форму')
+  }
 }
